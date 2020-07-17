3cc9c891dd6271da286c301dfed094c02ff7dd66
alfonso domenici <alfonso@alfonsodomenici.it>
2020-07-17 22:27:12 +0200

    qrcode

--- src/confirmation.js
+++ src/confirmation.js
@@ -3,6 +3,8 @@ import Menu from './Menu.js';
 import EventStore from './EventStore.js';
 import { html, render } from './lib/lit-html.js';
 import { dFull } from './fmt.js';
+import qrcode from './lib/qrcode-generator-es6.js';
+
 const updateBreadcrumb = ({ id, quando }) => {
     const bc = document.querySelector('nav.breadcrumb');
     const template = html`
@@ -28,6 +30,7 @@ const renderEvent = (evt) => {
         </div>
         <div class="card-content">
             <div class="content">
+                <p><strong>Ore </strong>  ${evt.ora}</p>
                 <p><strong>${evt.categoria}</strong></p>
                 <p class="subtitle">${evt.titolo}</p>
             </div>
@@ -67,12 +70,26 @@ const renderDynamic = (data) => {
         `;
     }else if (code === null){
         template = html`
+            ${renderQRCode(data.code)}
             <button @click=${e => onPrint(e)} class="button is-primary">Stampa</button>
+            <button @click=${e => onAnnulla(e)} class="button is-danger" title = "Annulla la prenotazione">Annulla</button>
+        `;
+    }else{
+        template = html`
             <button @click=${e => onAnnulla(e)} class="button is-danger" title = "Annulla la prenotazione">Annulla</button>
         `;
     }
     render(template, view);
 
+}
+
+const renderQRCode = (code) => {
+    console.log('render qrcode');
+    const el = document.querySelector('div.qrcode');
+    const qr = new qrcode(0, 'H');
+    qr.addData(`${document.location.href}&code=${code}`);
+    qr.make();
+    el.innerHTML = qr.createSvgTag({});
 }
 
 const renderAnnullata = _ => {
@@ -96,6 +113,34 @@ const renderError = msg => {
         </div>
     `;
     render(template, not);
+}
+
+const renderInfo = _ => {
+    const info = document.querySelector('div.info');
+    const template = html`
+        <div class="field">
+            <div class="control">
+                <label class="checkbox">
+                    <input type="checkbox" checked>
+                    <p class="is-3 is-size-4 has-text-weight-medium">Informativa sul trattamento dei dati personali</p>
+                    <p>Gentile interessato desideriamo informarLa che il “Regolamento Europeo 2016/679 relativo alla protezione delle persone fisiche con riguardo al Trattamento dei Dati Personali, nonché alla libera circolazione di tali dati” prevede la tutela delle persone e di altri soggetti rispetto al trattamento dei dati personali. CINEMAMBIENTE pertanto, in qualità di “Titolare” del trattamento, ai sensi dell’articolo 13 del GDPR, contattabile all’indirizzo XXXXXXXXXx, numero di telefono XXXXXXXXXX,  indirizzo e-mail XXXXXXXXxx , Le fornisce le seguenti informazioni:</p>
+                    <p>CINEMAMABIENTE tratterà i dati personali, sensibili o sanitari che saranno inseriti nel form direttamente dall’interessato. Il trattamento dei suoi dati ha come base giuridica quella di prenotare la sua partecipazione ad uno degli eventi previsti nell’ambito della manifestazione CINEMAMBIENTEVALCHIUSELLA in conseguenza dell’applicazione delle norme anticontagio COVID (DPCM 14/07/2020), che prevedono il mantenimento dei dati in archivio per 14 giorni dall’evento e la comunicazione di tali dati eventualmente, su richiesta, alle autorità sanitarie (ASL). I dati raccolti non saranno trasferiti al di fuori dell’Unione europea. L’interessato ha diritto ha richiedere la modifica dei dati semplicemente contattando il numero di telefono o la mail del titolare del trattamento. Se i dati indicati con asterisco non verranno conferiti, la prenotazione non potrà essere accettata.</p>
+                </label>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <label class="checkbox">
+                    <input type="checkbox" checked>
+                    <p class="is-3 is-size-4 has-text-weight-medium">Dichiarazione dell’interessato</p>
+                    <p>DICHIARO DI ESSERE A CONOSCENZA CHE SE IL GIORNO DELL’EVENTO AVRO’ LA FEBBRE OLTRE 37.5 °C, OPPURE ALTRI SINTOMI INFLUENZALI, NON PARTECIPERO’ ALL’EVENTO; INOLTRE, DICHIARO CHE PARTECIPERO’  ALL’EVENTO SOLO SE NEI PRECEDENTI 14 GIORNI NON AVRO’ AVUTO CONTATTI CON SOGGETTI POSITIVI AL COVID-19 E NON SARO’ STATO NEI PAESI A RISCHIO (RIF. SITO MINISTERO DEGLI AFFARI ESTERI – www.viaggiaresicuri.it).</p>
+                    <p>IN CASO DI QUALSIASI IMPEDIMENTO A PARTECIPARE, CANCELLERO’ LA PRENOTAZIONE CHIAMANDO IL N. xxxxxxxxxxxxX OPPURE VIA MAIL ALL’INDIRIZZO : xxxxxxxxxxxxxxxxxxx.</p>
+                </label>
+            </div>
+        </div>    
+    `;
+    render(template,info)
 }
 
 const onCloseError = e => {
@@ -125,7 +170,11 @@ const onAnnulla = (e) => {
 }
 
 const onPrint = (e) => {
+    document.querySelectorAll('button')
+        .forEach(b => b.classList.toggle('is-hidden'));
     window.print();
+    document.querySelectorAll('button')
+        .forEach(b => b.classList.toggle('is-hidden'));
 }
 
 const url = new URL(document.location.href);
@@ -142,4 +191,5 @@ store.find(eventId)
     .then(json => {
         renderPerson(json);
         renderDynamic(json);
+        renderInfo();
     });
